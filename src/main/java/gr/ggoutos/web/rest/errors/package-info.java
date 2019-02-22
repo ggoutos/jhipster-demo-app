ac075ea6170f3021bc333eb96e957b7af19c9cde
George Goutos <ggoutos@outlook.com>
2019-02-22 19:37:41 +0200

    Initial application generated by JHipster-5.8.1

---
+++ src/main/java/gr/ggoutos/web/rest/errors/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Specific errors used with Zalando's "problem-spring-web" library.
+ *
+ * More information on https://github.com/zalando/problem-spring-web
+ */
+package gr.ggoutos.web.rest.errors;
